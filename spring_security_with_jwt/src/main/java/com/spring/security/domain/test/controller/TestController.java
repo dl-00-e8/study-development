0cbffcd66848674197bfa53e9bda69588d6f7b81
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2024-06-21 19:19:43 +0900

    refactor: 미사용 import 삭제

--- spring_security_with_jwt/src/main/java/com/spring/security/domain/test/controller/TestController.java
+++ spring_security_with_jwt/src/main/java/com/spring/security/domain/test/controller/TestController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
