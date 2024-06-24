7dbb2ddf472cf36ea2bc57aa0d2a493d120dc6ee
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2024-06-24 14:13:51 +0900

    refactor: 이름 변경 후, 미사용 파일 삭제

--- spring_security_with_jwt/src/main/java/com/spring/security/domain/test/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.security.domain.test.repository;
-
-import com.spring.security.domain.test.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
