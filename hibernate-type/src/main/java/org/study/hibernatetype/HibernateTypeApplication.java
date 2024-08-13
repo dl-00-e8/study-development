c0704e299241e8d4ed3e5d05abb3310a825c8ae8
dl-00-e8 <sso07012@gmail.com>
2024-08-13 23:02:16 +0900

    feat: Json 타입 사용 예시

---
+++ hibernate-type/src/main/java/org/study/hibernatetype/HibernateTypeApplication.java
@@ -0,0 +1,13 @@
+package org.study.hibernatetype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateTypeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateTypeApplication.class, args);
+    }
+
+}
